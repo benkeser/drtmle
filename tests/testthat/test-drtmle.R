4f98e594bbe2e19f8f8d2a0c7bfe24b2cadf1758
David Benkeser <benkeser@berkeley.edu>
2017-07-12 17:26:23 -0400

    build develop branch in travis. fix testthat

--- tests/testthat/test-drtmle.R
+++ tests/testthat/test-drtmle.R
@@ -1,4 +1,4 @@
-install.packages("~/Dropbox/R/drtmle",repos=NULL,type="source")
+# install.packages("~/Dropbox/R/drtmle",repos=NULL,type="source")
 library(drtmle)
 
 context("Testing drtmle function")
